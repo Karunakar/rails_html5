e8431498ad2af4f0295f41e8eaceebf72cf00e98
Karunakar (Ruby) <revurikarna@gmail.com>
2011-09-02 17:46:31 +0530

    html5 rials app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sas::Application.config.secret_token = '46465c8663414322c4fc2557da66ad2fddbed11577e59ae7ba97c8f6cc41d685b0969ea187f35f81f72533460c860883a8d0464e822a36a480c35c224d094b9e'
